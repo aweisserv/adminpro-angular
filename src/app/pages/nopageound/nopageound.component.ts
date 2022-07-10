9a8e49c819943f92437daaec7ab443031ad801f7
Allan Weisser <vredes@live.com>
2022-07-09 21:31:15 -0400

    first commit

---
+++ src/app/pages/nopageound/nopageound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopageound',
+  templateUrl: './nopageound.component.html',
+  styleUrls: ['./nopageound.component.css']
+})
+export class NopageoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
